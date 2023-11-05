2ae720302f850d9044ad13aafa30a823039bc012
Navdeep Singh <navdeeprthr10@gmail.com>
2023-11-05 20:44:05 +0530

    connect database

--- api/index.js
+++ api/index.js
@@ -2,6 +2,16 @@ import express from 'express';
 // "type": "module"   ---->   Important to run express server inside api
 // as default type is common.js so in that case you need to use require instead of import
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mongoose from 'mongoose';
+mongoose.connect(process.env.MONGO).then(()=>{
+        console.log("Connected to MongoDB");
+    }).catch((err)=>{
+        console.log(err);
+});
+
 
 // nodemon ---> don't need to start the server again for any update
 // but not using it in production
